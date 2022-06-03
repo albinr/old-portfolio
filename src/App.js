a64c6f4c543607db2133641f3bee37372fdafa7e
Albin Ryberg <albin@rybergs.net>
2022-06-03 16:05:52 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import Home from "./pages";
+import {ThemeProvider} from "styled-components";
+import  {useDarkMode} from "./components/useDarkMode"
+import { GlobalStyles } from "./components/globalStyles";
+import { lightTheme, darkTheme } from "./components/Theme"
+import { Toggle } from "./components/Toggler";
 
 function App() {
+  const [theme, themeToggler] = useDarkMode();
+  const themeMode = theme === 'light' ? lightTheme : darkTheme;
+
   return (
-    <Router>
-      <Home />
-    </Router>
+    <ThemeProvider theme={themeMode}>
+      <>
+        <GlobalStyles/>
+        <Router>
+          <Home />
+        </Router>
+      </>
+    </ThemeProvider>
   );
 }
 
--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,71 +1,16 @@
-import Video from "../../videos/video.mp4";
-import Image from "../../images/albin.jpg";
-
+// import Image from "../../images/albin.jpg";
 import {
   HeroContainer,
-  HeroBg,
-  VideoBg,
-  HeroContent,
-  Header,
-  Text,
-  Title,
-  Name,
-  Intro,
-  ImageContainer,
-  ImageAr,
-  Buttons,
-  Underline,
 } from "./HeroElements";
 import { ScrollButton } from "../ScrollButtonElements";
 import { Button } from "../ButtonElements";
+import Toggle from "../Toggler";
 
 const HeroSection = () => {
   return (
     <HeroContainer id="home">
-      <HeroBg>
-        <VideoBg autoPlay loop muted src={Video} type="video/mp4" />
-      </HeroBg>
-      <HeroContent>
-        <Header>
-          <Text>
-            <Title>
-              <h1>Hello,</h1>
-            </Title>
-            <Name>
-              <h2>
-                my name is <Underline>Albin Ryberg</Underline>.
-              </h2>
-            </Name>
-            <Intro>
-              <h2>
-                Front-end developer studying at&nbsp;
-                <Underline>KYH</Underline>.
-              </h2>
-            </Intro>
-          </Text>
-          <ImageContainer>
-            <ImageAr src={Image} alt="Albin" type="image/jpg" />
-          </ImageContainer>
-        </Header>
-        <Buttons>
-          <ScrollButton
-            to="about"
-            smooth={true}
-            spy={true}
-            duration={500}
-            exact="true"
-            offset={-80}
-            primary
-            dark
-          >
-            More
-          </ScrollButton>
-          <Button>
-            Download CV
-          </Button>
-        </Buttons>
-      </HeroContent>
-      
+      <div>hello</div>
+      <Toggle/>
     </HeroContainer>
   );
 };
