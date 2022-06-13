f8b20692bee45fcebaecd958f2586b49b978247e
Albin Ryberg <albin@rybergs.net>
2022-06-13 21:56:09 +0200

    hello

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,11 @@
 import React, {useState} from "react";
-// import AboutSection from "../components/AboutSection";
 import Sidebar from "../components/Sidebar";
 import HeroSection from "../components/HeroSection";
 import Navbar from "../components/Navbar";
 import PortfolioSection from "../components/PortfolioSection";
 import ContactSection from "../components/ContactSection";
 import Footer from "../components/Footer";
-// import MobileNavbar from "../components/MobileNavbar";
+
 const Home = () => {
   const [isOpen, setIsOpen] = useState(false);
 
